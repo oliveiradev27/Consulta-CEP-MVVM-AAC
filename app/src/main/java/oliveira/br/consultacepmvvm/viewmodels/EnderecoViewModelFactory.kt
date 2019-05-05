9d286650a5d0f1d9720c078b9396681a02b2917b
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-05-05 18:51:26 -0300

    Remove ViewModelFactory que se tornou desnecessária com o uso do Dagger

--- app/src/main/java/oliveira/br/consultacepmvvm/viewmodels/EnderecoViewModelFactory.kt
+++
@@ -1,9 +0,0 @@
-package oliveira.br.consultacepmvvm.viewmodels
-
-import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.ViewModelProvider
-import oliveira.br.consultacepmvvm.repository.EnderecoRepository
-
-class EnderecoViewModelFactory(private val repository: EnderecoRepository) : ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T = EnderecoViewModel(repository) as T
-}
