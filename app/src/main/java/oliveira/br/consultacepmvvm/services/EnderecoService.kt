6b07cc6a0ce55618dce777b29332cdc886f0c562
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-05-05 17:59:38 -0300

    Separa lógica de consulta do listener para ser usado pelo Snackbar

--- app/src/main/java/oliveira/br/consultacepmvvm/services/EnderecoService.kt
+++ app/src/main/java/oliveira/br/consultacepmvvm/services/EnderecoService.kt
@@ -5,7 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-
 interface EnderecoService {
 
     @GET("{cep}/json/")
--- app/src/main/java/oliveira/br/consultacepmvvm/ui/activities/MainActivity.kt
+++ app/src/main/java/oliveira/br/consultacepmvvm/ui/activities/MainActivity.kt
@@ -1,16 +1,16 @@
 package oliveira.br.consultacepmvvm.ui.activities
 
 import android.arch.lifecycle.Observer
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import oliveira.br.consultacepmvvm.viewmodels.EnderecoViewModel
 import oliveira.br.consultacepmvvm.R
 import oliveira.br.consultacepmvvm.application.MyApplication
 import oliveira.br.consultacepmvvm.models.Endereco
 import oliveira.br.consultacepmvvm.ui.fragments.EnderecoFragment
+import oliveira.br.consultacepmvvm.viewmodels.EnderecoViewModel
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         MyApplication.graph.inject(this)
+
         viewModel
             .getEndereco()
             .observe(this, Observer<Endereco>{ endereco ->
@@ -34,13 +35,17 @@ class MainActivity : AppCompatActivity() {
                 configuraExibicaoEndereco(endereco) })
 
         btn_cep.setOnClickListener{
-            if (!et_cep.text.isNullOrEmpty()) {
-                viewModel.consultar(et_cep.text.toString())
-                showProgressDialog()
-            } else {
-                et_cep.error = getString(R.string.campo_obrigatorio)
-                et_cep.requestFocus()
-            }
+            consultarCep()
+        }
+    }
+
+    private fun consultarCep() {
+        if (!et_cep.text.isNullOrEmpty()) {
+            viewModel.consultar(et_cep.text.toString())
+            showProgressDialog()
+        } else {
+            et_cep.error = getString(R.string.campo_obrigatorio)
+            et_cep.requestFocus()
         }
     }
 
@@ -67,6 +72,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun showSearchError() {
         Snackbar.make(main_content, getString(R.string.dados_nao_encontrados), Snackbar.LENGTH_LONG)
-            .setAction(getString(R.string.tentar_novamente)) { btn_cep.performClick() }.show()
+            .setAction(getString(R.string.tentar_novamente)) { consultarCep() }.show()
     }
 }
